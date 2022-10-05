678af8eeccc2853b1bc173ef3546f4e0cfb64a5b
NovichenkoYura <novichenko.y@gmail.com>
2022-10-05 18:21:11 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, Link} from 'react-router-dom';
+import {Main} from './pages/Main';
+import {Explore} from './pages/Explore';
+import {Subscribers} from './pages/Subscribers';
+import {Subscriptions} from './pages/Subscriptions';
+import {Messages} from './pages/Messages';
+import {Saved} from './pages/Saved';
+import {Settings} from './pages/Settings';
+import {NotFoundpage} from './pages/NotFoundpage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <Link to='/'>Main</Link>
+      <Link to='/explore'>Explore</Link>
+      <Link to='/subscribers'>Subscribers</Link>
+      <Link to='/subscription'>Subscriptions</Link>
+      <Link to='/messages'>Messages</Link>
+      <Link to='/saved'>Saved</Link>
+      <Link to='/settings'>Settings</Link>
+      <Link to='/notfound'>NotFoundpage</Link>
+    </header>
+    <Routes>
+      <Route path="/" element={< Main/>}/>
+      <Route path="/explore" element={< Explore/>}/>
+      <Route path="/subscribers" element={< Subscribers/>}/>
+      <Route path="/subscription" element={< Subscriptions/>}/>
+      <Route path="/messages" element={< Messages/>}/>
+      <Route path="/saved" element={< Saved/>}/>
+      <Route path="/settings" element={< Settings/>}/>
+      <Route path="*" element={< NotFoundpage/>}/>
+    </Routes>
+    </>
+    
+      
+    
   );
 }
 
