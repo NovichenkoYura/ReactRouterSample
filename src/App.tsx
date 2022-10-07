ef1af088369a27817627777fd5075c361b3a89e1
NovichenkoYura <novichenko.y@gmail.com>
2022-10-07 17:38:35 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,24 @@ import {Saved} from './pages/Saved';
 import {Settings} from './pages/Settings';
 import {NotFoundpage} from './pages/NotFoundpage';
 
+import{Layout} from './components/Layout'
+
 
 function App() {
   return (
     <>
-    <header>
-      <Link to='/'>Main</Link>
-      <Link to='/explore'>Explore</Link>
-      <Link to='/subscribers'>Subscribers</Link>
-      <Link to='/subscription'>Subscriptions</Link>
-      <Link to='/messages'>Messages</Link>
-      <Link to='/saved'>Saved</Link>
-      <Link to='/settings'>Settings</Link>
-      <Link to='/notfound'>NotFoundpage</Link>
-    </header>
+    
     <Routes>
-      <Route path="/" element={< Main/>}/>
-      <Route path="/explore" element={< Explore/>}/>
-      <Route path="/subscribers" element={< Subscribers/>}/>
-      <Route path="/subscription" element={< Subscriptions/>}/>
-      <Route path="/messages" element={< Messages/>}/>
-      <Route path="/saved" element={< Saved/>}/>
-      <Route path="/settings" element={< Settings/>}/>
-      <Route path="*" element={< NotFoundpage/>}/>
+      <Route path="/" element ={<Layout/>}>
+        <Route index element={< Main/>}/>
+        <Route path="explore" element={< Explore/>}/>
+        <Route path="subscribers" element={< Subscribers/>}/>
+        <Route path="subscription" element={< Subscriptions/>}/>
+        <Route path="messages" element={< Messages/>}/>
+        <Route path="saved" element={< Saved/>}/>
+        <Route path="settings" element={< Settings/>}/>
+        <Route path="*" element={< NotFoundpage/>}/>
+      </Route>
     </Routes>
     </>
     
---
+++ src/components/Customlink.tsx
@@ -0,0 +1,25 @@
+import { Link, useMatch } from "react-router-dom";
+
+interface CustomLinkProps {
+children: string;
+to: string;
+}
+
+
+const CustomLink: React.FC< CustomLinkProps> = ({children, to, ...props} )=>{
+const match = useMatch(to);
+console.log({match})
+
+    return(
+      <Link 
+        to={to} 
+        style={{color: match ? 'var(--color-active)' : 'white'}}
+        {...props}>
+        {children}
+      </Link>
+         
+   
+    )
+}
+
+export {CustomLink}
